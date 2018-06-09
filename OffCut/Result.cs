b0474723619ffd67cd78539b8fbacc85d331e1ae
Tsepennikov Gregory <mcgrg@yandex.ru>
2018-06-09 18:24:28 +0300

    Some changes, evening 09.06.2018

--- OffCut/Result.cs
+++ OffCut/Result.cs
@@ -8,20 +8,35 @@ namespace OffCut
 {
     class Result
     {
+
+        //
+        //Расчет количества изделий, 2 варианта размещения изделий на сырье вдоль и поперек
+        //
         public static int Calc (Rectangle big, Rectangle small)
         {
+            //
+            //Расчет размещения вдоль
+            //
             int first = big.Lenght / small.Lenght;
             int second = big.Width / small.Width;
             int third = 0;
+            //
+            //Расчет возможности раскроя остатков
+            //
             if (big.Lenght % small.Lenght >= small.Width)
             {
                 third = ((big.Lenght % small.Lenght) / small.Width) * (big.Width / small.Lenght);
             }
             int res1 = first * second + third;
-
+            //
+            //Расчет размещения поперек
+            //
             first = big.Lenght / small.Width;
             second = big.Width / small.Lenght;
             third = 0;
+            //
+            //Расчет возможности раскроя остатков
+            //
             if (big.Width % small.Lenght >= small.Width)
             {
                 third = ((big.Width % small.Lenght) / small.Width) * (big.Lenght / small.Lenght);
